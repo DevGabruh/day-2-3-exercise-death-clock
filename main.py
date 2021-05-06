cc716a449773f0c12a64057991cdbc0262cd64e2
DevGabruh <>
2021-05-06 04:28:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# a: age m: months w: weeks d:days y:years
+a = int (age)
+y = (90 - a)
+m = int (y*12)
+w = int (y*52)
+d = int (y*365)
+print(f"Months Remaining {m}, weeks remaining {w}, days remaining {d}")
+
+
+
+
